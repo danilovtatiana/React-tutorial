df169232e2ca3ce0b813d26f0eee47d6c0494d51
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-05-23 21:08:39 +0300

    JSX rules

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 //stateless functional component or dumb component 
-//always return JSX (HTML)
+//always return JSX (HTML )
 
-function Greeting(){
-  return (<div>
-    <h1>
-      hello world
-    </h1>
-  </div>
-  );
-} 
+// function Greeting(){
+//   return (<div>
+//     <h1>
+//       hello world
+//     </h1>
+//   </div>
+//   );
+// } 
 
 // const Greeting = () => {
 //   return React.createElement(
@@ -20,5 +20,26 @@ function Greeting(){
 //      React.createElement('h1', {}, 'hello world') 
 //     );
 // };
+
+//JSX always returns a single element: div, section, fragment
+//use camelCase for html attribute
+// classname instead of class
+//close every element (even image)
+//formatting
+
+function Greeting(){
+  return (
+    <div>
+      <article>hello world</article> 
+      <ul>
+        <li>
+          <a href='#'>hello world</a>
+        </li>
+        <img src='' alt=''></img>
+        <input type='text' name='' id=''></input>
+      </ul>
+    </div>
+  );
+}
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
