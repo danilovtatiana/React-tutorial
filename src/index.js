d21d196504ede2484ac945bc818a464e4882eb49
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-05-23 20:49:59 +0300

    Update index.js
    first component in detail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//stateless functional component or dumb component 
+//always return JSX (HTML)
+
 function Greeting(){
-  return <h4>This is John and this is my first component</h4>
-};
+  return (<div>
+    <h1>
+      hello world
+    </h1>
+  </div>
+  );
+} 
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//      {},
+//      React.createElement('h1', {}, 'hello world') 
+//     );
+// };
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
